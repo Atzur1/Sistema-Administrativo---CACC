0aa10b2bdf35e19fe90e1f0fc10eb3f0145a38c1
Joel Galera <yoelgalera77@gmail.com>
2026-08-30 09:23:38 -0300

    feat: componentes hijos del portal administrativo y rutas configuradas

---
+++ frontend-cacc/src/app/admin-portal/actividad-movimientos/actividad-movimientos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-actividad-movimientos',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './actividad-movimientos.html',
+    styleUrl: './actividad-movimientos.css',
+})
+export class ActividadMovimientos {}
---
+++ frontend-cacc/src/app/admin-portal/deudas-morosidad/deudas-morosidad.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-deudas-morosidad',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './deudas-morosidad.html',
+    styleUrl: './deudas-morosidad.css',
+})
+export class DeudasMorosidad {}
